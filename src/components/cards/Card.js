75a2aac1cefedf6902f259e423b6c0605f5fc36c
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-03-05 16:50:04 +0530

    2 files modified

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -7,13 +7,23 @@ export default function Card({ item }) {
         width: '33%',
         padding: 10,
         minWidth: '300',
+        backgroundColor: '#d0d0d0',
       }}
     >
       <>
         <img src="https://picsum.photos/seed/picsum/200/300" />
         <div>{item.title}</div>
         <div>Price: {item.price}</div>
-        <div style={{ position: 'absolute', top: 0, right: 0 }}>
+        <div
+          style={{
+            position: 'absolute',
+            top: 20,
+            right: 20,
+            backgroundColor: 'orange',
+            padding: '5px',
+            borderRadius: 20,
+          }}
+        >
           {item.discount}
         </div>
       </>
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Card from './Card';
 export default function Cards({ title, data }) {
   return (
-    <div>
-      <h1>{title}</h1>
-      <div style={{ flexDirection: 'row', margin: 20 }}>
+    <div style={{ margin: 20 }}>
+      <h1>{title.toUpperCase()}</h1>
+      <div
+        style={{
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
         {data &&
           data.map((item) => {
             return <Card item={item} />;
