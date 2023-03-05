acc15be4911dd327507090b3795b6fef48ae00c2
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-03-05 15:28:07 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './style.css';
 import { Routes, Route } from 'react-router-dom';
-import { Login } from './pages';
+import Login from './pages/signup/Login';
 export default function App() {
   return (
     <div>
       <Routes>
-        <Route path="/*" exact component={<Login />}>
-          <Login />
-        </Route>
+        <Route path="/*" element={<Login />}></Route>
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import { BrowserRouter as Router } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
