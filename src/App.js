aaa7f86cbc382803094540acd39dca732b602b43
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-03-05 15:42:51 +0530

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import './style.css';
 import { Routes, Route } from 'react-router-dom';
 import { Login, Register, Forgot, Reset } from './pages/';
+import { Header, Footer } from './components/';
 export default function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="reset" element={<Reset />}></Route>
         <Route path="forgot" element={<Forgot />}></Route>
@@ -12,6 +14,7 @@ export default function App() {
         <Route path="register" element={<Register />}></Route>
         <Route path="/*" exact element={<Login />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
